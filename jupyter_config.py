bdc038377b001436450c683d34446a508ae46325
Tony Fast <tony.fast@gmail.com>
2020-04-19 21:41:08 -0400

    add a starter

--- jupyter_config.py
+++ jupyter_config.py
@@ -1,2 +1,14 @@
+from pathlib import Path
+
 c.LabBuildApp.minimize = True
 c.LabBuildApp.dev_build = False
+
+c.StarterManager.extra_starters = {
+    "whitepaper-single": {
+        "type": "copy",
+        "label": "Lesson 01",
+        "description": "Start Here",
+        "src": "notebooks/Lesson 01.ipynb",
+        "icon": Path("docs/logo.svg").read_text()
+    }
+}
